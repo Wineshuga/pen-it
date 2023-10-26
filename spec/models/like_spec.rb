6c67534bb08f48f7111787849b6406b94dd23197
Wineshuga <uzochukwuwinnie@gmail.com>
2023-10-27 00:03:39 +0100

    Add tests for like model

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:post) { create(:post) }
+  let(:like) { create(:like, user: user, post: post) }
+
+  it "is valid with valid attributes" do
+    expect(like).to be_valid
+  end
+
+  it "belongs to a user" do
+    expect(like.user).to eq(user)
+  end
+
+  it "belongs to a post" do
+    expect(like.post).to eq(post)
+  end
+
+  it "increments the likes_counter of the associated post after save" do
+    expect { like.save }.to change { post.reload.likes_counter }.by(1)
+  end
 end
