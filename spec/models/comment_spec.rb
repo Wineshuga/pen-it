d1231867e985155474149848f3b345376e326f8a
Wineshuga <uzochukwuwinnie@gmail.com>
2023-10-27 00:06:40 +0100

    Add tests for comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:post) { create(:post) }
+  let(:comment) { create(:comment, user: user, post: post) }
+
+  it "is valid with valid attributes" do
+    expect(comment).to be_valid
+  end
+
+  it "belongs to a user" do
+    expect(comment.user).to eq(user)
+  end
+
+  it "belongs to a post" do
+    expect(comment.post).to eq(post)
+  end
+
+  it "increments the comments_counter of the associated post after save" do
+    expect { comment.save }.to change { post.reload.comments_counter }.by(1)
+  end
+
 end
