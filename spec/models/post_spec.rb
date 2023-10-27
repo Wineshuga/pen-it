c88e09ba11e07f3f840c02d19b304d28531abe59
Wineshuga <uzochukwuwinnie@gmail.com>
2023-10-27 12:50:23 +0100

    Fix errors in post model test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,52 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  let(:user) { create(:user) }
-  let(:post) { create(:post, author: user) }
+  let(:user) { User.new(name: 'Tom', photo: 'tom.jpg', bio: 'Tom\'s bio', posts_counter: 0) }
+  let(:subject) { Post.new(author: user, title: 'My first post', text: 'post', comments_counter: 0, likes_counter: 0) }
 
-  it 'is valid with valid attributes' do
-    expect(post).to be_valid
+  describe 'validations' do
+    before { subject.save }
+
+    it 'is valid with the required attributes' do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid when comments_counter is not an integer' do
+      subject.comments_counter = 'a'
+      expect(subject).not_to be_valid
+    end
+
+    it 'is not valid when comments_counter is less than zero' do
+      subject.comments_counter = -10
+      expect(subject).not_to be_valid
+    end
+
+    it 'is not valid when likes_counter is not an integer' do
+      subject.likes_counter = 'a'
+      expect(subject).not_to be_valid
+    end
+
+    it 'is not valid when likes_counter is less than zero' do
+      subject.likes_counter = -1
+      expect(subject).not_to be_valid
+    end
   end
+  describe 'methods' do
+    it 'updates posts_counter' do
+      subject.send(:update_posts_counter)
+      expect(user.posts_counter).to eq(1)
+    end
 
-  it 'must have a title' do
-    post.title = nil
-    expect(post).not_to be_valid
-  end
-
-  it 'title length must be more than 250 characters' do
-    post.title = 'a' * 251
-    expect(post).not_to be_valid
-  end
-
-  it 'comments_counter must be greater than or equal to 0' do
-    post.comments_counter = 0
-    expect(post).to be_valid
-  end
-
-  it 'is not valid with comments_counter less than 0' do
-    post.comments_counter = -1
-    expect(post).not_to be_valid
-  end
-
-  it 'likes_counter must be greater than or equal to 0' do
-    post.likes_counter = 0
-    expect(post).to be_valid
-  end
-
-  it 'is not valid with likes_counter less than 0' do
-    post.likes_counter = -1
-    expect(post).not_to be_valid
-  end
-
-  it 'returns the 5 most recent comments' do
-    comments = create_list(:comment, 10, post:)
-    recent_comments = post.recent_comments
-
-    expect(recent_comments.count).to eq(5)
-    expect(recent_comments).to eq(comments[0..4].sort_by(&:created_at).reverse)
-  end
-
-  it "increments the author's posts_counter after save" do
-    expect { post.save }.to change { user.reload.posts_counter }.by(1)
+    it 'returns recent comments' do
+      6.times do
+        Comment.create(user:, post: subject, text: 'This is the comment')
+      end
+      comments = subject.recent_comments
+      expect(comments.length).to eq(5)
+    end
   end
 end
