c35aa385fb3b486b61e06ceced84cb97bee70c96
Wineshuga <uzochukwuwinnie@gmail.com>
2023-11-04 13:29:14 +0100

    Update post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,29 @@ class PostsController < ApplicationController
   def show
     @posts = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author = User.find(params[:user_id])
+    @post.likes_counter = 0
+    @post.comments_counter = 0
+
+    if @post.save
+      flash[:notice] = 'Post created successfully!'
+      redirect_to user_posts_path(id: @post.id)
+    else
+      flash[:alert] = 'Error: could not create a new post'
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
