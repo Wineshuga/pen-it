65a36f01cf437fa81e40ffb355212113615d6f0a
Wineshuga <uzochukwuwinnie@gmail.com>
2023-11-08 22:18:17 +0100

    Modify posts counter validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'user_id'
 
   validates :name, presence: true
-  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
-
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nil: true }
+  
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
