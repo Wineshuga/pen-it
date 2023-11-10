5154be3d7a5a6d148ed04925cbc23780fabc82a8
Wineshuga <uzochukwuwinnie@gmail.com>
2023-11-11 00:32:49 +0100

    Modify accessibilty

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,15 +4,13 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :read, Post
-    return unless user.present?
-    can :create, Post
-    can :create, Comment
-    can :create, Like
-    can :destroy, Post, author_id: user.id
-    can :destroy, Comment, author_id: user.id
-    can :destroy, Like, author_id: user.id 
-    return unless user.admin?
-    can :manage, all
+    can :read, :all
+    if user.admin?
+      can :manage, :all
+      else
+        can :create, [Post, Comment]
+        can :destroy, Post, author_id: user.id
+        can :destroy, Comment, user_id: user.id
+    end
   end
 end
