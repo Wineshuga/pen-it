8f4e89a0b6e65a5027659a8c7517fbc66def6700
Wineshuga <uzochukwuwinnie@gmail.com>
2023-11-11 00:32:20 +0100

    Create admin method

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,14 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'user_id'
   has_many :posts, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'user_id'
-
-  enum role: [:user, :admin]
   
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nil: true }
+
+
+  def admin?
+    role == "Admin"
+  end
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
