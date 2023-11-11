e3adaea01163950da0580073a10893e9d1e20614
Wineshuga <uzochukwuwinnie@gmail.com>
2023-11-11 12:30:14 +0100

    Update routes with api routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,13 @@ Rails.application.routes.draw do
     end
   end
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :users, only: [:show] do
+      resources :posts, only: [:index] do
+        resources :comments, only: [:index, :create]
+      end
+    end
+  end
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
